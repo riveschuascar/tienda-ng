89c4278380dcf37b0874d966ba367e9b15b5029b
Huascar Rivero <huascar.rivero@ucb.edu.bo>
2025-05-12 17:50:43 -0400

    feat: componentes para las paginas

---
+++ src/app/elements/redcircle/redcircle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redcircle',
+  imports: [],
+  templateUrl: './redcircle.component.html',
+  styleUrl: './redcircle.component.scss'
+})
+export class RedcircleComponent {
+
+}
