3c1f01f5c97687952e55360c6d7b36a697fd302d
Huascar Rivero <huascar.rivero@ucb.edu.bo>
2025-05-12 17:52:29 -0400

    feat: redireccion con link base y para 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { StoreComponent } from './pages/store/store.component';
 import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'aboutus', component: AboutusComponent },
-    { path: 'store', component: StoreComponent }
+    { path: 'store', component: StoreComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: '**', component: NotfoundComponent}
 ];
