cf4e69890eb42e9f72c2c8558cfeda233bb0669b
melanytorrez <torrezmelany13@gmail.com>
2025-06-03 00:07:56 -0400

    agregando estilos al home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { GreetingComponent } from '../../elements/greeting/greeting.component';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent],
+  standalone: true,
+  imports: [CommonModule, RouterModule, GreetingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent { }
 
-}
+
+
