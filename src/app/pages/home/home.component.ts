f9e2938ef5feef6f745be3cd73cd67152269df37
vivi <revollovivian@gmail.com>
2025-06-04 15:43:13 -0330

    estilos del home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
-import { GreetingComponent } from '../../elements/greeting/greeting.component';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ProductoService } from '../../services/producto.service';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],   //, GreetingComponent
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+  productoService = inject(ProductoService);
+  cartService = inject(CartService);
+  productosDestacados: Product[] = [];
+
+  ngOnInit() {
+    this.productoService.obtenerProductos().subscribe({
+      next: productos => this.productosDestacados = productos.slice(0, 2),  // Puedes usar filtro si quieres destacar según precio, categoría, etc.
+      error: e => console.error(e)
+    });
+  }
+
+  addToCart(producto: Product) {
+    this.cartService.addToCart(producto);
+    alert('Producto agregado al carrito');
+  }
+}
+
 
 
 
