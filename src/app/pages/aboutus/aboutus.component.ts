bfa433c11a1de2c5f4a2b931b17a2074e2fe35b9
vivi <revollovivian@gmail.com>
2025-06-04 15:31:49 -0330

    About us

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-aboutus',
-  imports: [],
   templateUrl: './aboutus.component.html',
-  styleUrl: './aboutus.component.scss'
+  styleUrls: ['./aboutus.component.scss']
 })
 export class AboutusComponent {
-
+ 
 }
